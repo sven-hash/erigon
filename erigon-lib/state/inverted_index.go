fe1c35edff2b11b1b106efe3931253c3b6adb0a1
awskii <artem.tsskiy@gmail.com>
2024-01-23 00:13:00 +0000

    ok

--- erigon-lib/state/inverted_index.go
+++ erigon-lib/state/inverted_index.go
@@ -459,6 +459,7 @@ func buildIdxFilter(ctx context.Context, d *compress.Decompressor, compressed Fi
 func (ii *InvertedIndex) BuildMissedIndices(ctx context.Context, g *errgroup.Group, ps *background.ProgressSet) {
 	for _, item := range ii.missedIdxFiles() {
 		item := item
+		fmt.Printf("buildMissedIndices (%s): %s %d-%d\n", item.decompressor.FileName(), ii.filenameBase, item.startTxNum/ii.aggregationStep, item.endTxNum/ii.aggregationStep)
 		g.Go(func() error {
 			return ii.buildEfi(ctx, item, ps)
 		})
